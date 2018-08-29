0137aa3e4a747aa3ce264bd41d83fe79ce4f2aea
PicklesAU <34080434+PicklesAU@users.noreply.github.com>
2018-08-29 12:55:34 +0700

    Update CustomAndroidFormatStrategy.java

--- src/android/CustomAndroidFormatStrategy.java
+++ src/android/CustomAndroidFormatStrategy.java
@@ -87,10 +87,13 @@ public class CustomAndroidFormatStrategy implements MediaFormatStrategy {
     }
 
     public MediaFormat createAudioOutputFormat(MediaFormat inputFormat) {  
+        return null;
+        /*
         final MediaFormat format = MediaFormat.createAudioFormat(MIMETYPE_AUDIO_AAC,
                 inputFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE), 2);
         format.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
         format.setInteger(MediaFormat.KEY_BIT_RATE, 128 * 1000);
         return format;
+        */
     }
 } 
