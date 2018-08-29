a68a5e09c18e3acbf30cc7096b5e85c33acd859d
PicklesAU <34080434+PicklesAU@users.noreply.github.com>
2018-08-29 12:12:36 +0700

    Update CustomAndroidFormatStrategy.java

--- src/android/CustomAndroidFormatStrategy.java
+++ src/android/CustomAndroidFormatStrategy.java
@@ -19,6 +19,7 @@ public class CustomAndroidFormatStrategy implements MediaFormatStrategy {
     private static final int LONGER_LENGTH = 1280;
     private static final int SHORTER_LENGTH = 720;
     private static final int DEFAULT_VIDEO_BITRATE = 8000 * 1000; // From Nexus 4 Camera in 720p
+    public static final String MIMETYPE_AUDIO_AAC = "audio/mp4a-latm";
     private final int mVideoBitrate;
     private final int mAudioBitrate;
     private final int mAudioChannels;
@@ -74,7 +75,7 @@ public class CustomAndroidFormatStrategy implements MediaFormatStrategy {
         if (mAudioBitrate == AUDIO_BITRATE_AS_IS || mAudioChannels == AUDIO_CHANNELS_AS_IS) return null;
 
         // Use original sample rate, as resampling is not supported yet.
-        final MediaFormat format = MediaFormat.createAudioFormat(MediaFormatExtraConstants.MIMETYPE_AUDIO_AAC,
+        final MediaFormat format = MediaFormat.createAudioFormat(MIMETYPE_AUDIO_AAC,
                 inputFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE), mAudioChannels);
         format.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
         format.setInteger(MediaFormat.KEY_BIT_RATE, mAudioBitrate);
