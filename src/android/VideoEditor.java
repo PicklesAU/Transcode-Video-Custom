ff2c17d5d7c4148ffe513f9141cf922379a16f43
PicklesAU <34080434+PicklesAU@users.noreply.github.com>
2018-08-30 07:35:00 +0700

    Update VideoEditor.java

--- src/android/VideoEditor.java
+++ src/android/VideoEditor.java
@@ -240,8 +240,12 @@ public class VideoEditor extends CordovaPlugin {
                     float videoWidth = Float.parseFloat(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
                     float videoHeight = Float.parseFloat(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
 
+                    /*
                     MediaTranscoder.getInstance().transcodeVideo(fin.getFD(), outputFilePath,
                             new CustomAndroidFormatStrategy(videoBitrate, fps, width, height), listener, videoDuration);
+                    */
+                    MediaTranscoder.getInstance().transcodeVideo(fin.getFD(), outputFilePath,
+                            MediaFormatStrategyPresets.createAndroid720pStrategy(8000 * 1000, 128 * 1000, 1), listener);
 
                 } catch (Throwable e) {
                     Log.d(TAG, "transcode exception ", e);
